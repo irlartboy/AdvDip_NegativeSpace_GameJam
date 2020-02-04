660863cb547bc96e3c94e15034f66d073fb7e47b
irlartboy <ollie_king99@outlook.com>
2020-02-04 14:50:54 +1100

    First

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    float hori;
+   
+
+    public int runSpeed = 10;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        hori = Input.GetAxisRaw("Horizontal"); 
+    }
+    void FixedIpdate()
+    {
+        body.velocity = new Vector2(hori * runSpeed, 0 );
+    }
+}
